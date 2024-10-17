7ff3fca5d9093683bdf5019d1a28c038b6a3b130
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2024-10-18 03:39:54 +0500

    Add files via upload

--- blynk_smart_cradle/blynk_smart_cradle.ino
+++ blynk_smart_cradle/blynk_smart_cradle.ino
@@ -18,9 +18,16 @@
 
 #define fan 18
 #define sound 12
-#define speed 23
-#define DHTTYPE DHT11   // DHT 11  
-#define DHTPIN 32     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11  // DHT 11
+#define DHTPIN 32      // Digital pin connected to the DHT sensor
+int ENA = 23;          //speed
+int IN1 = 5;           //motor pin 1
+int IN2 = 4;
+;  //motor pin 2
+const int frequency = 500;
+const int pwm_channel = 0;
+const int resolution = 8;
+
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -36,14 +43,14 @@ char pass[] = "humayunsj789";
 #define AUTHOR_EMAIL "smartcradle5@gmail.com"
 #define AUTHOR_PASSWORD "qsgrmmijxjcujobk"
 /* Recipient's email*/
-#define RECIPIENT_EMAIL "humayunsiraj0@gmail.com"//enter email to send alert
-#define emailSenderAccount    "smartcradle5@gmail.com"    // Sender email address
-#define emailSenderPassword  "qsgrmmijxjcujobk"       // Sender email password
-#define smtpServer            "smtp.gmail.com"
-#define smtpServerPort        465
-#define emailSubject          "CRADLE ALERT"   // Email subject
+#define RECIPIENT_EMAIL "humayunsiraj0@gmail.com"    //enter email to send alert
+#define emailSenderAccount "smartcradle5@gmail.com"  // Sender email address
+#define emailSenderPassword "qsgrmmijxjcujobk"       // Sender email password
+#define smtpServer "smtp.gmail.com"
+#define smtpServerPort 465
+#define emailSubject "CRADLE ALERT"  // Email subject
 
-String inputMessage ="humayunsiraj0@gmail.com";   // Recipient email alert
+String inputMessage = "humayunsiraj0@gmail.com";  // Recipient email alert
 String enableEmailChecked = "checked";
 String inputMessage2 = "true";
 // Flag variable to keep track if email notification was sent or not
@@ -56,7 +63,7 @@ BlynkTimer timer;
 bool fan_blynk;
 bool fan_state;
 bool active;
-
+int speed;
 
 
 
@@ -64,12 +71,6 @@ int statostart;
 int relay1_state = 0;
 
 
-
-// BLYNK_CONNECTED()
-// {
-//   Blynk.syncVirtual(button1_vpin);
-
-// }
 
 #define timeSeconds 10
 
@@ -79,40 +80,52 @@ const int led = 2;
 
 // Timer: Auxiliary variables
 unsigned long now = millis();
-unsigned long lastTrigger = 0;
-boolean startTimer = false;
-
+unsigned long pt_speed;
 // Checks if motion was detected, sets LED HIGH and starts a timer
 
 
 
 
 //fan control
-BLYNK_WRITE(V1) {
+BLYNK_WRITE(V2) {
   fan_blynk = param.asInt();
   Serial.print("fan BLYNK:");
   Serial.println(fan_blynk);
- 
+}
+
+BLYNK_WRITE(V1) {
+  speed = param.asInt();
+  Serial.print("speed:");
+  Serial.println(speed);
 }
 
 
 
-void setup()
-{
+void setup() {
 
   Serial.begin(115200);
   softwareSerial.begin(9600);
   // pinMode(PIR, INPUT_PULLUP);
-  digitalWrite(rev_dir, LOW);
-  pinMode(forward_dir, OUTPUT);
-  digitalWrite(forward_dir, LOW);
-  
-  analogWrite(speed,100);
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
-    dht.begin();
-   Blynk.begin(auth, ssid, pass);
+
+  pinMode(fan, OUTPUT);
+  digitalWrite(fan, LOW);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  ledcSetup(pwm_channel, frequency, resolution);
+  ledcAttachPin(ENA, pwm_channel);
+  ledcWrite(pwm_channel, 0);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+
+
+
+  dht.begin();
+  Blynk.begin(auth, ssid, pass);
   timer.setInterval(100L, DHT11sensor);
+  timer.setInterval(100L, fan_cntrl);
 }
 
 
@@ -122,13 +135,32 @@ void loop()
   Blynk.run();
   timer.run();
   now = millis();
-  
+
   cry = digitalRead(sound);
   cry_ctime = millis();
+  if (millis() - pt_speed > 500) {
+    pt_speed = millis();
+    if (speed == 100) {
+      digitalWrite(IN1, 1);
+      digitalWrite(IN2, 0);
+      ledcWrite(pwm_channel, 255);
+    } else if (speed == 35) {
+      digitalWrite(IN1, 1);
+      digitalWrite(IN2, 0);
+      ledcWrite(pwm_channel, 200);
+    } else if (speed == 70) {
+      digitalWrite(IN1, 1);
+      digitalWrite(IN2, 0);
+      ledcWrite(pwm_channel, 225);
+    } else {
+      digitalWrite(IN1, 0);
+      digitalWrite(IN2, 0);
+      ledcWrite(pwm_channel, 0);
+    }
+  }
 
-  
 
-  if (cry_ctime - cry_ptime > 1000) {
+  if (cry_ctime - cry_ptime > 2000) {
     cry_ptime = cry_ctime;
     Serial.print("NO OF SAMPLES ");
     Serial.println(samples);
@@ -136,8 +168,7 @@ void loop()
       if (sendEmailNotification("BABY IS CRYING")) {
         Serial.println("BABY is CRYING");
         emailSent = true;
-      }
-      else {
+      } else {
         Serial.println("Email failed to send");
       }
       cry_alert++;
@@ -146,9 +177,8 @@ void loop()
     if (isCry == 1 & s_cnt < 10) {
       ++s_cnt;
       Serial.println(s_cnt);
-    }
-    else if (samples < 1000 |s_cnt==10) {
-      s_cnt=0;
+    } else if (samples < 1000 | s_cnt == 10) {
+      s_cnt = 0;
       cry_alert = 0;
       isCry = 0;
     }
@@ -164,28 +194,28 @@ void loop()
 float h;
 float t;
 void DHT11sensor() {
-  h =h>100?0:++h; //dht.readHumidity();
-  t =t>100?0:++t;; //dht.readTemperature();
- // h =dht.readHumidity();
- // t =dht.readTemperature();
+  /// h =h>100?0:++h; //dht.readHumidity();
+  // t =t>100?0:++t;; //dht.readTemperature();
+  h = dht.readHumidity();
+  t = dht.readTemperature();
 
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-    Blynk.virtualWrite(V2, t);  // Send temperature to V2 on the Blynk app
-  
+  Blynk.virtualWrite(V0, t);  // Send temperature to V2 on the Blynk app
+}
+void fan_cntrl() {
+  digitalWrite(fan, fan_blynk);
 }
 
-void servo_cntrl()
-{
+void servo_cntrl() {
   if (digitalRead(forward_btn) == LOW) {
     delay(200);
     if (toy_active) {
       servo.write(90);
       Serial.println("SERVO WRITE +++");
-
     }
     //Serial.print("forward = ");
     //Serial.println();
@@ -202,9 +232,6 @@ void servo_cntrl()
     //Serial.println();
     //delay(50);
   }
-
-
-
 }
 
 bool sendEmailNotification(String emailMessage) {
@@ -226,8 +253,7 @@ bool sendEmailNotification(String emailMessage) {
   smtpData.addRecipient(inputMessage);
   smtpData.setSendCallback(sendCallback);
 
-  if (!MailClient.sendMail(smtpData))
-  {
+  if (!MailClient.sendMail(smtpData)) {
     Serial.println("Error sending Email, " + MailClient.smtpErrorReason());
     return false;
   }
@@ -236,14 +262,12 @@ bool sendEmailNotification(String emailMessage) {
   return true;
 }
 
-void sendCallback(SendStatus msg)
-{
+void sendCallback(SendStatus msg) {
   // Print the current status
   Serial.println(msg.info());
 
   // Do something when complete
-  if (msg.success())
-  {
+  if (msg.success()) {
     Serial.println("----------------");
   }
 }
